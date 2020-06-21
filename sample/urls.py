433e22939f621783cea24f53c07c90b7d2268a01
yura <yurasavin@gmail.com>
2020-06-21 19:00:38 +0300

    update root urls.py

--- sample/urls.py
+++ sample/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from rest_framework import routers
 
+from sample.aggregated_usage.views import (AggregatedUsageByDateView,
+                                           AggregatedUsageByPriceView)
 from sample.att_subscriptions.views import ATTSubscriptionViewSet
 from sample.plans.views import PlanViewSet
 from sample.purchases.views import PurchaseViewSet
@@ -26,12 +27,14 @@ from sample.sprint_subscriptions.views import SprintSubscriptionViewSet
 
 router = routers.DefaultRouter()
 
-router.register(r'att_subscriptions', ATTSubscriptionViewSet)
+router.register(r'aggregated-usage-by-price', AggregatedUsageByPriceView, basename='aggregated-usage-by-price')
+router.register(r'aggregated-usage-by-date', AggregatedUsageByDateView, basename='aggregated-usage-by-date')
+router.register(r'att-subscriptions', ATTSubscriptionViewSet)
 router.register(r'plans', PlanViewSet)
 router.register(r'purchases', PurchaseViewSet)
-router.register(r'sprint_subscriptions', SprintSubscriptionViewSet)
+router.register(r'sprint-subscriptions', SprintSubscriptionViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api/', include((router.urls, 'api'), namespace='api')),
+    path('api/v1/', include((router.urls, 'api'), namespace='api')),
 ]
